75032862ea5404cb8991861c1aad5377dda008f1
jb cazaux <jbcazaux@gmail.com>
2020-10-20 14:39:00 +0200

    typo test DiceRoll

--- TPs/solutions/tests/mock/app/DiceRoll.test.js
+++ TPs/solutions/tests/mock/app/DiceRoll.test.js
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react'
 import DiceRoll from './DiceRoll'
 
 jest.mock('./NiceDice', () => () => <div>NiceDice</div>)
-jest.mock('components/button/Button', () => ({children}) => <div>{children}</div>)
-describe('NiceDice', () => {
+jest.mock('components/button/Button', () => ({ children }) => <div>{children}</div>)
+describe('DiceRoll', () => {
   it('renders DiceRoll', () => {
     const { container } = render(<DiceRoll />)
 
