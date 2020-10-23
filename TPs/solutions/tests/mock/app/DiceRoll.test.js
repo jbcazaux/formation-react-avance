cc195e6c12450f5f5e1539fd6952199549f8d79c
jbcazaux <jean-baptiste.cazaux@externe.maif.fr>
2020-10-23 14:39:45 +0200

    prettier

--- TPs/solutions/tests/mock/app/DiceRoll.test.js
+++ TPs/solutions/tests/mock/app/DiceRoll.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import DiceRoll from './DiceRoll'
 
 jest.mock('./NiceDice', () => () => <div>NiceDice</div>)
-jest.mock('components/button/Button', () => ({children}) => <div>{children}</div>)
+jest.mock('components/button/Button', () => ({ children }) => <div>{children}</div>)
 describe('DiceRoll', () => {
   it('renders DiceRoll', () => {
     const { container } = render(<DiceRoll />)
