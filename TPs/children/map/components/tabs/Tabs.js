8c7033e4200b501f1393e2d0656dfdbaf56c504c
jbcazaux <jbcazaux@gmail.com>
2022-05-08 03:29:56 +0200

    children.map

--- TPs/children/map/components/tabs/Tabs.js
+++ TPs/children/map/components/tabs/Tabs.js
@@ -5,16 +5,16 @@ import Tab from 'components/tabs/Tab'
 import cn from 'classnames'
 
 const Tabs = ({ children }) => {
-  // TODO : Créer un state pour sauvegarder l'index de l'onglet sélectionné
+  // TODO 1: Créer un state pour sauvegarder l'index de l'onglet sélectionné
 
   return (
     <div>
       <div className={style.tabs}>
         {/* Affichage des titres des onglets */}
-        {React.Children.map(children, child => {
+        {React.Children.map(children, (child, index) => {
           if (child.type !== Tab) return null
           return (
-            // TODO : <TabTitle onClick={mettre à jour l index sélectionné} selected={?}>TITRE</TabTitle>
+            // TODO 2: <TabTitle onClick={mettre à jour l index sélectionné} selected={?}>TITRE</TabTitle>
             null
           )
         })}
@@ -22,8 +22,8 @@ const Tabs = ({ children }) => {
       {/* Affichage de l'onglet actif (c'est à dire un des <Tab /> ) */}
       {React.Children.map(children, child => {
         if (child.type !== Tab) return null
-        // TODO : Ne pas afficher les onglets non actifs (ceux qui n'ont pas le bon index)
-        return null // TODO : Retourner l'onglet, c'est à dire l'élément sur lequel on itère
+        // TODO 3 : Ne pas afficher les onglets non actifs (ceux qui n'ont pas le bon index)
+        return null // TODO 4 : Retourner l'onglet, c'est à dire l'élément sur lequel on itère
       })}
     </div>
   )
@@ -37,7 +37,7 @@ export default Tabs
 
 const TabTitle = ({ selected, onClick }) => (
   <div className={cn(style.tab, { [style.selectedTab]: selected })} onClick={onClick}>
-    TITRE {/* TODO : comment récuperer le titre passé par le parent ?*/}
+    TITRE {/* TODO 5: comment récuperer le titre de la props title du composant <Tab />? */}
   </div>
 )
 
